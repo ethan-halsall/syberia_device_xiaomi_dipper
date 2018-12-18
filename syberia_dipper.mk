249022a9c41c4d15256c39ef1d5eff571be2a285
ethan-halsall <ethan.halsall@gmail.com>
2018-12-18 15:11:52 +0000

    Bring up syberia

---
+++ syberia_dipper.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/dipper/device.mk)
+
+# Include Syberia common configuration
+include vendor/syberia/config/common_full_phone.mk
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := syberia_dipper
+PRODUCT_DEVICE := dipper
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 8
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.11.0.OEACNFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEACNFA release-keys" \
+    PRODUCT_NAME="dipper" \
+    TARGET_DEVICE="dipper"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+ add_lunch_combo syberia_dipper-userdebug
+ add_lunch_combo syberia_dipper-eng
